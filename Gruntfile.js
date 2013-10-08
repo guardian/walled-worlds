59494529c0415c9b4e1fb603eeb241f963b4f041
Andrew Mason <andrew.mason@guardian.co.uk>
2013-10-08 13:07:10 +0100

    Fix deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -134,7 +134,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-copy");
-  grunt.loadNpmTasks('grunt-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-mustache');
