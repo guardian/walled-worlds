b66495711b7558ec2c1d8acb264df94e8fe7fc74
seanclarkeguardian <sean.clarke@guardian.co.uk>
2013-11-07 10:21:55 +0000

    removing red maps

--- src/js/app/models/config.js
+++ src/js/app/models/config.js
@@ -1,6 +1,6 @@
 define({
   google_spreadsheet_key: '0AjNAJ9Njg5YTdGtEZVdreHpBN3ZFOFJVVDdLUXhEcmc',
-  debug: true,
+  debug: false,
   useGoogleMaps: true,
   google_analytics_uid: 'UA-25353554-18'
 });
