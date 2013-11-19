efeca70f55bc719918101ab533963c856b8a6399
Andrew Mason <andrew.mason@guardian.co.uk>
2013-11-19 11:40:30 +0000

    Fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -262,7 +262,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-s3');
 
 
-  grunt.registerTask("default", ["clean", "copy", "mustache", "requirejs:devs", "sass", "replace:dev", "connect", "watch"]);
+  grunt.registerTask("default", ["clean", "copy", "mustache", "requirejs:dev", "sass", "replace:dev", "connect", "watch"]);
   grunt.registerTask("build", ["clean", "copy", "mustache", "requirejs:prod", "sass", "replace:prod"]);
   grunt.registerTask("deploy", ["build", "s3:production"]);
   grunt.registerTask("test-deploy", ["build", "s3:test"]);
